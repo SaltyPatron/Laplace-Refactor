465b1e9716616a4d4eeb5e260d5771bc0d3e40db
SaltyPatron <thesaltypatron@gmail.com>
2026-09-05 23:43:10 -0500

    test: register structured grammar-provider acceptance

--- tests/decomposition_orchestration.cmake
+++ tests/decomposition_orchestration.cmake
@@ -11,6 +11,16 @@ target_compile_options(laplace_decomposition_orchestration_tests PRIVATE
 gtest_discover_tests(laplace_decomposition_orchestration_tests
     PROPERTIES LABELS "implementation;decomposition;recipe;recursive;grammar")
 
+add_executable(laplace_decomposition_structured_provider_tests
+    "${CMAKE_CURRENT_LIST_DIR}/decomposition_structured_provider_tests.cpp")
+target_link_libraries(laplace_decomposition_structured_provider_tests PRIVATE
+    Laplace::Decomposition
+    GTest::gtest_main)
+target_compile_options(laplace_decomposition_structured_provider_tests PRIVATE
+    $<$<CXX_COMPILER_ID:GNU,Clang>:-Wall;-Wextra;-Wpedantic;-Werror;-Wconversion;-Wshadow>)
+gtest_discover_tests(laplace_decomposition_structured_provider_tests
+    PROPERTIES LABELS "implementation;decomposition;recipe;grammar;ast;structure;witness")
+
 add_library(laplace_decomposition_witness_identity_mutant STATIC
     "${CMAKE_CURRENT_LIST_DIR}/../engine/src/decomposition_composition.cpp")
 target_include_directories(laplace_decomposition_witness_identity_mutant PRIVATE
