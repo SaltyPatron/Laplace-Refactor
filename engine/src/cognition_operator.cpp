2de23c98b40e226bd7f0baa936e16e8bf4a9006e
SaltyPatron <thesaltypatron@gmail.com>
2026-09-11 16:55:29 -0500

    Bind live observation estate target compiler into engine

--- engine/src/cognition_operator.cpp
+++ engine/src/cognition_operator.cpp
@@ -5,6 +5,7 @@
 #include "cognition_operator_part04.inc"
 #include "target_compile.inc"
 #include "target_scope_plan.inc"
+#include "target_observation_compile.inc"
 #include "target_factorization.inc"
 #include "target_attention_projection.inc"
 #include "target_package.inc"
