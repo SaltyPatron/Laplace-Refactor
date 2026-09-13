9ef332deb342f7472d61fd6d742733aa46182693
SaltyPatron <thesaltypatron@gmail.com>
2026-09-13 18:10:27 -0500

    Compile cognition WHY_NOT outcome path

--- engine/src/observation_query.cpp
+++ engine/src/observation_query.cpp
@@ -13,8 +13,9 @@
 #include "cognition_realization.inc"
 #include "cognition_realization_composition.inc"
 #include "cognition_materialization.inc"
+#include "cognition_why_not.inc"
 #include "cognition_conversation.inc"
 #include "cognition_interpreted_conversation.inc"
 #include "cognition_prompt_admission.cpp"
 #include "cognition_prompt_structural_provider.inc"
-#include "cognition_prompt_conversation.inc"
+#include "cognition_prompt_conversation.inc"}
