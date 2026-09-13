bbcd2b6977076a90d6cb59b2086c164683d70898
SaltyPatron <thesaltypatron@gmail.com>
2026-09-13 18:17:34 -0500

    Fix cognition outcome include compilation

--- engine/src/observation_query.cpp
+++ engine/src/observation_query.cpp
@@ -18,4 +18,4 @@
 #include "cognition_interpreted_conversation.inc"
 #include "cognition_prompt_admission.cpp"
 #include "cognition_prompt_structural_provider.inc"
-#include "cognition_prompt_conversation.inc"}
+#include "cognition_prompt_conversation.inc"
