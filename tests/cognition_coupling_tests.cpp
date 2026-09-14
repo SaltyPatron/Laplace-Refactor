fdc6a3977a85c002ac9d463460fc2568839b5d73
SaltyPatron <thesaltypatron@gmail.com>
2026-09-14 03:52:34 -0500

    Add coupling response-field acceptance tests

---
+++ tests/cognition_coupling_tests.cpp
@@ -0,0 +1,13 @@
+#include "laplace/cognition_coupling.h"
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "cognition_coupling_tests_part00.inc"
+#include "cognition_coupling_tests_part01.inc"
+#include "cognition_coupling_tests_part02.inc"
