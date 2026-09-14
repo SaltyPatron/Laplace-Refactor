b40e6dcc55525bcb69c898faa762e0ff8eb51064
SaltyPatron <thesaltypatron@gmail.com>
2026-09-14 03:37:30 -0500

    Add cognition coupling contract skeleton

---
+++ engine/include/laplace/cognition_coupling.h
@@ -0,0 +1,21 @@
+#ifndef LAPLACE_COGNITION_COUPLING_H
+#define LAPLACE_COGNITION_COUPLING_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include "laplace/cognition_observation_request.h"
+#include "laplace/export.h"
+#include "laplace/types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define LAPLACE_COGNITION_COUPLING_VERSION UINT32_C(1)
+
+typedef struct laplace_cognition_coupling_result laplace_cognition_coupling_result;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
