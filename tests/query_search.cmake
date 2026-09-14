02c2ea18dccf9030e5367b605f6b9d3b3cf80813
SaltyPatron <thesaltypatron@gmail.com>
2026-09-14 04:52:30 -0500

    Register cognition response-field tests

--- tests/query_search.cmake
+++ tests/query_search.cmake
@@ -19,6 +19,7 @@ add_executable(laplace_observation_query_tests
     "${PROJECT_SOURCE_DIR}/tests/cognition_standing_operator_tests.cpp"
     "${PROJECT_SOURCE_DIR}/tests/cognition_standing_packet_tests.cpp"
     "${PROJECT_SOURCE_DIR}/tests/cognition_observation_provider_set_capacity_tests.cpp"
+    "${PROJECT_SOURCE_DIR}/tests/cognition_response_field_tests.cpp"
     "${PROJECT_SOURCE_DIR}/tests/cognition_semantic_act_tests.cpp"
     "${PROJECT_SOURCE_DIR}/tests/cognition_interpretation_tests.cpp"
     "${PROJECT_SOURCE_DIR}/tests/cognition_discourse_tests.cpp"
