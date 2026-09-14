443fd1ecc6134464bfb9eae735b77a5d944b4fa2
SaltyPatron <thesaltypatron@gmail.com>
2026-09-14 03:58:34 -0500

    Add prompt-wide orientation acceptance test

---
+++ tests/cognition_prompt_orientation_tests.cpp
@@ -0,0 +1,17 @@
+#include "laplace/cognition_prompt_conversation.h"
+#include "laplace/identity.h"
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "context_fixture.h"
+#include "prompt_admission_fixture.h"
+
+#include "cognition_prompt_orientation_tests_part00.inc"
+#include "cognition_prompt_orientation_tests_part01.inc"
+#include "cognition_prompt_orientation_tests_part02.inc"
